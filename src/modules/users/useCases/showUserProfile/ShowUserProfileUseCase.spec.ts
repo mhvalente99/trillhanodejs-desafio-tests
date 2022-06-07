73a626717741acda91bf9716ccbe2383797068a4
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-06-07 20:55:33 -0300

    test: config test to show user profile

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,11 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+describe("Show User Profile - Use Case", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository);
+  })
+})
