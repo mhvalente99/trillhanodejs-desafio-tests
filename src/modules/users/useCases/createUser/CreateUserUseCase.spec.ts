d9e7b2735c6c38936d78d56ba831936dfd0679de
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-06-07 20:50:44 -0300

    test: ensure to not create a new user with same email

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let inMemoryUsersRepository: InMemoryUsersRepository;
@@ -19,5 +20,21 @@ describe("Create User - Use Case", () => {
     });
 
     expect(user).toHaveProperty("id");
+  })
+
+  it("should not be able to create a new user with same email", async () => {
+    await createUserUseCase.execute({
+      email: "sameemail@teste.com.br",
+      name: "teste",
+      password: "1234"
+    });
+
+    await expect(
+      createUserUseCase.execute({
+        email: "sameemail@teste.com.br",
+        name: "teste same",
+        password: "1234"
+      })
+    ).rejects.toBeInstanceOf(CreateUserError);
   })
 })
