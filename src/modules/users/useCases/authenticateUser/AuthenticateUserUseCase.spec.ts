8c7cfcbdc3714f8893afe7c11edcebe5bd151b8d
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-06-07 21:59:11 -0300

    test: create test to authenticate user

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
@@ -8,5 +9,22 @@ describe("Authenticate User - Use Case", () => {
   beforeEach(() => {
     inMemoryUsersRepository = new InMemoryUsersRepository();
     authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able to authenticate user", async () => {
+    const hashPassword = await hash("teste123", 8);
+
+    await inMemoryUsersRepository.create({
+      email: "teste@teste.com.br",
+      name: "teste",
+      password: hashPassword
+    });
+
+    const authenticatedUser = await authenticateUserUseCase.execute({
+      email: "teste@teste.com.br",
+      password: "teste123"
+    });
+
+    expect(authenticatedUser).toHaveProperty("token");
   });
 });
