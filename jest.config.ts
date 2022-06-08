396573238d56d72d1e25fceb54e5a02c906206c0
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-06-07 21:15:45 -0300

    config: remove controller coverage while not create tests end2end

--- jest.config.ts
+++ jest.config.ts
@@ -26,7 +26,7 @@ export default {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["/node_modules/", "I*DTO.ts?$"],
+  coveragePathIgnorePatterns: ["/node_modules/", "I*DTO.ts?$", "^.*Controller.ts?$"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
