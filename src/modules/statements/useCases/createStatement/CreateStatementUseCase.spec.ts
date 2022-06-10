3dc26fbedbc7bcb5a028f7e3bf05e5bed0f82ffd
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-06-10 18:53:59 -0300

    test: create test to create a new statement deposit

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { OperationType } from "../../entities/Statement";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository"
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
@@ -12,5 +13,22 @@ describe("Create Statement - Use Case", () => {
     inMemoryUsersRepository = new InMemoryUsersRepository();
 
     createStatementUseCase = new CreateStatementUseCase(inMemoryUsersRepository, inMemoryStatementsRepository);
-  })
+  });
+
+  it("should be able to create a new statement deposit", async () => {
+    const user = await inMemoryUsersRepository.create({
+      email: "test-deposit@test.com.br",
+      name: "Deposit Teste",
+      password: "123"
+    });
+
+    const statement = await createStatementUseCase.execute({
+      amount: 100,
+      description: "Deposit",
+      type: OperationType.DEPOSIT,
+      user_id: user.id!
+    });
+
+    expect(statement).toHaveProperty("id");
+  });
 })
