af468e90c301e638b2f861bf048348b411843a89
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-06-10 19:34:42 -0300

    test: create test to return statement operation

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { OperationType } from "../../entities/Statement";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase";
 
@@ -13,5 +14,27 @@ describe("Get Statement Operation - Use Case", () => {
     inMemoryStatementsRepository = new InMemoryStatementsRepository();
 
     getStatementOperationUseCase = new GetStatementOperationUseCase(inMemoryUsersRepository, inMemoryStatementsRepository);
+  });
+
+  it("should be able to return statement operation", async () => {
+    const user = await inMemoryUsersRepository.create({
+      email: "statement@test.com.br",
+      name: "Statement Operation",
+      password: "123"
+    });
+
+    const statement = await inMemoryStatementsRepository.create({
+      amount: 100,
+      description: "Test Statement",
+      type: OperationType.DEPOSIT,
+      user_id: user.id!
+    });
+
+    const statementOperation = await getStatementOperationUseCase.execute({
+      statement_id: statement.id!,
+      user_id: user.id!
+    });
+
+    expect(statementOperation).toHaveProperty("id");
   });
 });
